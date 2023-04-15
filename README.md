6d9893a515c4627d032d866cb459f051799a82ff
Damjan Pavlica <mudroljub@gmail.com>
2023-04-15 16:32:00 +0200

    fix BUG: menjanje slike nazad

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 ## TODO
 
-- popuniti strane sadržajem
-- BUG: menjanje slike nazad
 - uvećati sliku na klik
 - stranica ilustracije
 - postaviti pozadinsku sliku
--- main.js
+++ main.js
@@ -7,10 +7,14 @@ comics = ['001', '002', '003', '004', '005', '006', '007', '008']
 let i = 0
 
 arrowRight.addEventListener('click', () => {
-  mainImage.src=`images/comics/bullo${comics[++i % comics.length]}.jpg`
+  i++
+  if (i >= comics.length) i = 0
+  mainImage.src=`images/comics/bullo${comics[i]}.jpg`
 })
 
 arrowLeft.addEventListener('click', () => {
-  i = i - 1 < 0 ? comics.length - 1 : i - 1
+  i--
+  if (i < 0) i = comics.length - 1
+  console.log(i)
   mainImage.src = `images/comics/bullo${comics[i]}.jpg`;
 });
