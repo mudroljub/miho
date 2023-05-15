b71a08d2565dbfbae2e30d0fb5380beedb1f12e2
Damjan Pavlica <mudroljub@gmail.com>
2023-05-15 11:10:02 +0200

    dodati novi strip

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 ## TODO
+
+- dodati swipe
+- umesto page owner info da piše
+OWNED BY : https://twitter.com/ILtroman?s=20
+- da stoji twiter ptičica preko linka
--- main.js
+++ main.js
@@ -2,21 +2,26 @@ const arrowLeft = document.getElementById('arrow-left')
 const arrowRight = document.getElementById('arrow-right')
 const mainImage = document.getElementById('main-image')
 
-const total = 9
-var audio = new Audio('gun.mp3');
+const total = 10
+const audio = new Audio('gun.mp3')
+
+const pad = (num, size = 3) => {
+  const s = "00" + num
+  return s.substring(s.length - size)
+}
 
 let i = 1
 
 arrowRight.addEventListener('click', () => {
   i++
   if (i > total) i = 1
-  mainImage.src=`images/comics/bullo 00${i}.jpg`
+  mainImage.src=`images/comics/bullo ${pad(i)}.jpg`
   audio.play();
 })
 
 arrowLeft.addEventListener('click', () => {
   i--
   if (i < 1) i = total
-  mainImage.src = `images/comics/bullo 00${i}.jpg`;
+  mainImage.src = `images/comics/bullo ${pad(i)}.jpg`;
   audio.play();
 });
