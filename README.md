53490ba86cb7fda078bc24e4d3826f2ed6d65a1d
Damjan Pavlica <mudroljub@gmail.com>
2023-05-15 11:56:37 +0200

    dodat owner

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 ## TODO
 
-- umesto page owner info da piše
-OWNED BY : https://twitter.com/ILtroman?s=20
-- da stoji twiter ptičica preko linka
--- main.js
+++ main.js
@@ -1,34 +1,50 @@
 const arrowLeft = document.getElementById('arrow-left')
 const arrowRight = document.getElementById('arrow-right')
 const mainImage = document.getElementById('main-image')
+const owner = document.getElementById('owner')
+
+const total = 10
+const audio = new Audio('gun.mp3')
+const owners = {
+  1: 'https://twitter.com/ILtroman',
+}
 
 let i = 1
 let touchstartX = 0
 let touchendX = 0
 
-const total = 10
-const audio = new Audio('gun.mp3')
-
 /* FUNCTIONS */
+
+const renderOwner = () => {
+  owner.innerHTML = owners[i] 
+    ? `OWNED BY: <a href="${owners[i]}" target="_blank"><img src="images/twitter.png" height="24"></a>`
+    : 'page owner info'
+}
 
 const pad = (num, size = 3) => {
   const s = "00" + num
   return s.substring(s.length - size)
 }
 
-const nextImage = () => {
-  if (++i > total) i = 1
+const update = () => {
   mainImage.src = `images/comics/bullo ${pad(i)}.jpg`
   audio.play()
+  renderOwner()
+}
+
+const nextImage = () => {
+  if (++i > total) i = 1
+  update()
 }
 
 const prevImage = () => {
   if (--i < 1) i = total
-  mainImage.src = `images/comics/bullo ${pad(i)}.jpg`
-  audio.play()
+  update()
 }
 
 /* EVENTS */
+
+renderOwner()
 
 arrowRight.addEventListener('click', nextImage)
 arrowLeft.addEventListener('click', prevImage)
